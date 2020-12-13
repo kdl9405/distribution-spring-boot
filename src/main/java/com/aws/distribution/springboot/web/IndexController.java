9abc357d29a077eceb78792eb6c97a836e55dac9
kdl9405 <sjaks1546@gmail.com>
2020-12-13 19:34:02 +0900

    게시글 수정, 삭제

--- src/main/java/com/aws/distribution/springboot/web/IndexController.java
+++ src/main/java/com/aws/distribution/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.aws.distribution.springboot.web;
 
 import com.aws.distribution.springboot.service.posts.PostsService;
+import com.aws.distribution.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,10 +22,16 @@ public class IndexController {
         return "index";
     }
 
-
-
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("posts",dto);
+
+        return "posts-update";
     }
 }
